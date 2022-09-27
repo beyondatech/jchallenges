835082c86dfa53377f66eab9c33cb19367ebfc24
libg <33592624+tobin4j@users.noreply.github.com>
2022-09-28 00:08:10 +0800

    Update SuccResource.java

--- src/main/java/beyondata/SuccResource.java
+++ src/main/java/beyondata/SuccResource.java
@@ -1,9 +1,48 @@
 package beyondata;
 
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/succ")
 public class SuccResource {
+
+    @POST
+    @Path("/{input}")
     public String index(String input) {
-        // TODO try to implement this endpoint to pass test case SuccResourceTest.
-        throw new UnsupportedOperationException();
+        String left = input.substring(0,1);
+        String right = input.substring(1);
+
+        right = next(right);
+        if (right.length() > 1){
+            left = next(left);
+            right = right.substring(1);
+        }
+
+        return left + right;
+
+
+
+    }
+
+    private String next(String str){
+
+        if ("a".equals(str)){
+            return "b";
+        }
+        if ("9".equals(str)){
+            return "10";
+        }
+        if ("z".equals(str)){
+            return "aa";
+        }
+        if ("A".equals(str)){
+            return "B";
+        }
+        if ("Z".equals(str)){
+            return "AA";
+        }
+        return null;
+
     }
 }
