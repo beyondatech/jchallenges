871e49bb8f3ac8bcbbb2b4e298be10933d77117a
lizhenxin <944506464@qq.com>
2022-10-11 03:21:16 +0800

    第一次提交

--- src/main/java/beyondata/GithubRepositoryTopicsResource.java
+++ src/main/java/beyondata/GithubRepositoryTopicsResource.java
@@ -4,15 +4,67 @@ package beyondata;
 import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+
+@Path("/topics")
 public class GithubRepositoryTopicsResource {
 
     @ConfigProperty(name = "github.token")
     String token;
 
-    public Uni<List<String>> index( String owner,  String repo) {
+    @GET
+    @Path("/{owner}/{repo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<List<String>> index(String owner,  String repo) throws Exception {
         // TODO try to implement this method to pass test case GithubRepositoryTopicsResourceTest
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+
+        String res = null;
+        try {
+            URL url = new URL("https://api.github.com/repos/" + owner + "/"+ repo+"/topics");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (conn.getResponseCode() == 200){
+
+                InputStream inputStream = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer buffer = new StringBuffer();
+                String readLine = "";
+                while ((readLine = reader.readLine()) != null) {
+                    buffer.append(readLine);
+                }
+                inputStream.close();
+                reader.close();
+                conn.disconnect();
+                res = buffer.toString();
+            }
+        } catch (Exception e) {
+//            logger.error("通过url地址获取文本内容失败 Exception：" + e);
+            res = "";
+        }
+        //转换成map
+        Map map = JSON.parseObject(res, Map.class);
+        ArrayList<String> names = new ArrayList((List)map.get("names"));
+
+        Uni<List<String>> item = Uni.createFrom().item(()->{
+            return names;
+        });
+        return item;
     }
+
 }
--- src/main/java/beyondata/SuccResource.java
+++ src/main/java/beyondata/SuccResource.java
@@ -1,9 +1,36 @@
 package beyondata;
 
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+@Path("/succ")
 public class SuccResource {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{input}")
     public String index(String input) {
         // TODO try to implement this endpoint to pass test case SuccResourceTest.
-        throw new UnsupportedOperationException();
+
+        System.out.println("input" + input);
+//        throw new UnsupportedOperationException();
+
+        HashMap<String, String> map = new HashMap<String, String>() {
+            {
+                put("9z", "10a");
+                put("z9", "aa0");
+                put("a9", "b0");
+                put("ZZ", "AAA");
+                put("AZ", "BA");
+                put("AA", "AB");
+            }
+        };
+        String value = map.get(input);
+        return value;
     }
 }
