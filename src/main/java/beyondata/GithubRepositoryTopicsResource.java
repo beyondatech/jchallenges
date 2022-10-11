eebb2e3b59abd91c0c74a24e29308c5d9f9bc02a
lizhenxin <944506464@qq.com>
2022-10-11 18:33:08 +0800

    第二次

--- src/main/java/beyondata/GithubRepositoryTopicsResource.java
+++ src/main/java/beyondata/GithubRepositoryTopicsResource.java
@@ -1,6 +1,8 @@
 package beyondata;
 
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.http.HttpUtil;
 import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -8,12 +10,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -32,33 +28,8 @@ public class GithubRepositoryTopicsResource {
     public Uni<List<String>> index(String owner,  String repo) throws Exception {
         // TODO try to implement this method to pass test case GithubRepositoryTopicsResourceTest
 //        throw new UnsupportedOperationException();
-
-        String res = null;
-        try {
-            URL url = new URL("https://api.github.com/repos/" + owner + "/"+ repo+"/topics");
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            if (conn.getResponseCode() == 200){
-
-                InputStream inputStream = conn.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuffer buffer = new StringBuffer();
-                String readLine = "";
-                while ((readLine = reader.readLine()) != null) {
-                    buffer.append(readLine);
-                }
-                inputStream.close();
-                reader.close();
-                conn.disconnect();
-                res = buffer.toString();
-            }
-        } catch (Exception e) {
-//            logger.error("通过url地址获取文本内容失败 Exception：" + e);
-            res = "";
-        }
-        //转换成map
-        Map map = JSON.parseObject(res, Map.class);
+        String getResult = HttpUtil.get(String.format("https://api.github.com/repos/%s/%s/topics", owner,repo));
+        Map map = JSON.parseObject(getResult, Map.class);
         ArrayList<String> names = new ArrayList((List)map.get("names"));
 
         Uni<List<String>> item = Uni.createFrom().item(()->{
